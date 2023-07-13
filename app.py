3634590035f4aa260f99d6d31125eb52d710ce4b
Giacomo Cirò <130163233+BrightManx@users.noreply.github.com>
2023-07-13 15:54:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,76 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# import requests
-# import matplotlib.pyplot as plt
-# from race_analysis import get_laps, get_positions, get_lapTimes, get_currentMetadata, get_results, get_standings
-
-# st.set_page_config(
-#     page_title="F1 Race Analysis",
-#     layout="wide",
-#     initial_sidebar_state="collapsed"
-# )
-
-# current_metadata = get_currentMetadata()
-# season, round = current_metadata.Season.item(), current_metadata.Round.item()
-# results = get_results(season, round)
-# driverStandings, constructorStandings = get_standings(season, round)
-# laps = get_laps()
-# positions = get_positions(laps)
-# lapTimes = get_lapTimes(laps)
-
-# # METADATA
-# st.write(f"<h1 style='text-align:center;'> Formula 1: \"{current_metadata.raceName.item()}\" Race Analysis </h1>", unsafe_allow_html=True)
-# st.write('---')
-
-# cols = st.columns(5)
-
-# with cols[0]:
-#     st.write(f'''      
-#     - Season: **{current_metadata.Season.item()}**  
-#     - Round: **{current_metadata.Round.item()}** 
-#     - Race Name: **{current_metadata.raceName.item()}**  
-#     - Circuit Name: **{current_metadata.circuitName.item()}** 
-#     - Country: **{current_metadata.country.item()}**  
-#     - Locality: **{current_metadata.locality.item()}**  
-#     ''')
-
-
-# with cols[1]:
-#     st.map(current_metadata[['lat', 'lon']].astype(float), zoom=2)
-
-# with cols[2]:
-#     st.write("<h4 style='text-align:center;'> Results </h4>", unsafe_allow_html=True)
-#     st.write(results[['Driver', 'points']])
-
-# with cols[3]:
-#     st.write("<h4 style='text-align:center;'> Drivers Standings </h4>", unsafe_allow_html=True)
-#     st.write(driverStandings[['Driver', 'points', 'wins']])
-
-# with cols[4]:
-#     st.write("<h4 style='text-align:center;'> Constructors Standings </h4>", unsafe_allow_html=True)
-#     st.write(constructorStandings[['Constructor', 'points', 'wins']])
-
-
-# #### PLOTS
-# st.write('---')
-# col1, col2 = st.columns(2)
-
-# with col1:
-#     st.write("<h3 style='text-align:center;'> Positions By Lap </h3>", unsafe_allow_html=True)
-#     positions_drivers = st.multiselect('Whose positions would you like to compare?', positions.columns)
-#     st.line_chart(positions, y = positions_drivers)
-
-# with col2:
-#     st.write("<h3 style='text-align:center;'> Lap Times By Lap </h3>", unsafe_allow_html=True)
-#     lapTimes_drivers = st.multiselect('Whose Lap Times would you like to compare?', lapTimes.columns)
-#     st.line_chart(lapTimes, y = lapTimes_drivers)
-
-
-# ###########
-# st.write('---')
-# st.write("<h4 style='text-align:center;'> new sections to come | work in progress... </h4>", unsafe_allow_html=True)
-
-
 import streamlit as st
 import pandas as pd
 import requests
@@ -101,10 +28,13 @@ if action == 'Most recent':
     metadata = lastMetadata
 
 if action == 'Custom':
-    season = st.slider('Select season', min_value = 1950, max_value=int(last_season), step=1, value=int(last_season))
-    seasonMetadata = get_seasonMetadata(season)
-    round = st.selectbox('Select round', seasonMetadata.raceName)
-    round = seasonMetadata.loc[seasonMetadata.raceName == round, 'Round'].item()
+    col1, col2 = st.columns(2)
+    with col1:
+        season = st.slider('Select season', min_value = 1950, max_value=int(last_season), step=1, value=int(last_season))
+        seasonMetadata = get_seasonMetadata(season)
+    with col2:
+        round = st.selectbox('Select round', seasonMetadata.raceName)
+        round = seasonMetadata.loc[seasonMetadata.raceName == round, 'Round'].item()
     metadata = get_roundMetadata(season, round)
 
 results = get_results(season, round)
@@ -114,7 +44,6 @@ positions = get_positions(laps)
 lapTimes = get_lapTimes(laps)
 
 # metadata
-st.write(metadata)
 st.write(f"<h1 style='text-align:center;'> {season} {metadata.raceName.item()}</h1>", unsafe_allow_html=True)
 st.write(f"<h5 style='text-align:center;'>results, analysis and standings</h5>", unsafe_allow_html=True)
 st.write('---')
